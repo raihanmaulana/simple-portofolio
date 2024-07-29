852edabd04774352da0b0d0eeca4c854b7ec7fbb
raihan <raihanmaulana09@gmail.com>
2024-07-29 22:17:44 +0700

    coba

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex items-center border-b-2 px-6 py-2 h-24">
+    <div className="flex items-center border-b-2 px-6 py-2 h-20">
       <h1 className="font-bold">NextJS APP</h1>
       <div className="grow ">
         <div
           className="flex items-center justify-end gap-2
         md:gap-8"
         >
-          <Link href="home">Home</Link>
+          <Link href="/">Home</Link>
           <Link href="projects">Projects</Link>
           <Link href="contact">Contact</Link>
           <Link href="about">About</Link>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,124 +1,333 @@
-import { PaperClipIcon } from "@heroicons/react/20/solid";
+// import { PaperClipIcon } from "@heroicons/react/20/solid";
+
+// export default function Example() {
+//   return (
+
+//     // <div className="container mx-auto">
+//     //   <div className="px-4 sm:px-0">
+//     //     <div className="text-4xl pt-8 pb-3 text-center font-bold ">
+//     //       Contact Me
+//     //     </div>
+//     //     <p className="mt-1 max-w-2xl text-white-500 pt-4">
+//     //       Personal details and application.
+//     //     </p>
+//     //   </div>
+//     //   <div className="mt-6 border-t border-white-100">
+//     //     <dl className="divide-y divide-white-100">
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           Full name
+//     //         </dt>
+//     //         <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
+//     //           Muhammad Raihan Maulana
+//     //         </dd>
+//     //       </div>
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           Application for
+//     //         </dt>
+//     //         <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
+//     //           Backend Developer
+//     //         </dd>
+//     //       </div>
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           Email address
+//     //         </dt>
+//     //         <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
+//     //           raihanmaulana09@gmail.com
+//     //         </dd>
+//     //       </div>
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           Salary expectation
+//     //         </dt>
+//     //         <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
+//     //           $120,000
+//     //         </dd>
+//     //       </div>
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           About
+//     //         </dt>
+//     //         <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
+//     //           Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+//     //           incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+//     //           consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+//     //           proident. Irure nostrud pariatur mollit ad adipisicing
+//     //           reprehenderit deserunt qui eu.
+//     //         </dd>
+//     //       </div>
+//     //       <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+//     //         <dt className="text-sm font-medium leading-6 text-white-900">
+//     //           Attachments
+//     //         </dt>
+//     //         <dd className="mt-2 text-sm text-white-900 sm:col-span-2 sm:mt-0">
+//     //           <ul
+//     //             role="list"
+//     //             className="divide-y divide-white-100 rounded-md border border-white-200"
+//     //           >
+//     //             <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+//     //               <div className="flex w-0 flex-1 items-center">
+//     //                 <PaperClipIcon
+//     //                   aria-hidden="true"
+//     //                   className="h-5 w-5 flex-shrink-0 text-white-400"
+//     //                 />
+//     //                 <div className="ml-4 flex min-w-0 flex-1 gap-2">
+//     //                   <span className="truncate font-medium">
+//     //                     CV Muhammad Raihan Maulana.pdf
+//     //                   </span>
+//     //                   <span className="flex-shrink-0 text-white-400">
+//     //                     GDrive
+//     //                   </span>
+//     //                 </div>
+//     //               </div>
+//     //               <div className="ml-4 flex-shrink-0">
+//     //                 <a
+//     //                   href="https://drive.google.com/file/d/1jkVaFygszqF77HEC6jEJ2pfnr4ojvhdV/view"
+//     //                   className="font-medium text-indigo-600 hover:text-indigo-500"
+//     //                 >
+//     //                   View
+//     //                 </a>
+//     //               </div>
+//     //             </li>
+//     //             <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+//     //               <div className="flex w-0 flex-1 items-center">
+//     //                 <PaperClipIcon
+//     //                   aria-hidden="true"
+//     //                   className="h-5 w-5 flex-shrink-0 text-white-400"
+//     //                 />
+//     //                 <div className="ml-4 flex min-w-0 flex-1 gap-2">
+//     //                   <span className="truncate font-medium">
+//     //                     coverletter_back_end_developer.pdf
+//     //                   </span>
+//     //                   <span className="flex-shrink-0 text-white-400">
+//     //                     4.5mb
+//     //                   </span>
+//     //                 </div>
+//     //               </div>
+//     //               <div className="ml-4 flex-shrink-0">
+//     //                 <a
+//     //                   href="#"
+//     //                   className="font-medium text-indigo-600 hover:text-indigo-500"
+//     //                 >
+//     //                   View
+//     //                 </a>
+//     //               </div>
+//     //             </li>
+//     //           </ul>
+//     //         </dd>
+//     //       </div>
+//     //     </dl>
+//     //   </div>
+//     // </div>
+
+//   );
+// }
+
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+"use client";
+
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Field, Label, Switch } from "@headlessui/react";
 
 export default function Example() {
+  const [agreed, setAgreed] = useState(false);
+
   return (
-    <div className="container mx-auto">
-      <div className="px-4 sm:px-0">
-        <div className="text-4xl pt-8 pb-3 text-center font-bold ">
-          About Me
-        </div>
-        <p className="mt-1 max-w-2xl text-white-500 pt-4">
-          Personal details and application.
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+        />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Contact sales
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Aute magna irure deserunt veniam aliqua magna enim voluptate.
         </p>
       </div>
-      <div className="mt-6 border-t border-white-100">
-        <dl className="divide-y divide-white-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              Full name
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
-              Muhammad Raihan Maulana
-            </dd>
+      <form
+        action="#"
+        method="POST"
+        className="mx-auto mt-16 max-w-xl sm:mt-20"
+      >
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              First name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="first-name"
+                name="first-name"
+                type="text"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              Application for
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
-              Backend Developer
-            </dd>
+          <div>
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Last name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="last-name"
+                name="last-name"
+                type="text"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              Email address
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
-              raihanmaulana09@gmail.com
-            </dd>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="company"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Company
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="company"
+                name="company"
+                type="text"
+                autoComplete="organization"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              Salary expectation
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
-              $120,000
-            </dd>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              About
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-              incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-              consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-              proident. Irure nostrud pariatur mollit ad adipisicing
-              reprehenderit deserunt qui eu.
-            </dd>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="phone-number"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Phone number
+            </label>
+            <div className="relative mt-2.5">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Country
+                </label>
+                <select
+                  id="country"
+                  name="country"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                >
+                  <option>US</option>
+                  <option>CA</option>
+                  <option>EU</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                />
+              </div>
+              <input
+                id="phone-number"
+                name="phone-number"
+                type="tel"
+                autoComplete="tel"
+                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-white-900">
-              Attachments
-            </dt>
-            <dd className="mt-2 text-sm text-white-900 sm:col-span-2 sm:mt-0">
-              <ul
-                role="list"
-                className="divide-y divide-white-100 rounded-md border border-white-200"
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Message
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+          </div>
+          <Field className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+              <Switch
+                checked={agreed}
+                onChange={setAgreed}
+                className="group flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 data-[checked]:bg-indigo-600"
               >
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      aria-hidden="true"
-                      className="h-5 w-5 flex-shrink-0 text-white-400"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        CV Muhammad Raihan Maulana.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-white-400">
-                        GDrive
-                      </span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="https://drive.google.com/file/d/1jkVaFygszqF77HEC6jEJ2pfnr4ojvhdV/view"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      View
-                    </a>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      aria-hidden="true"
-                      className="h-5 w-5 flex-shrink-0 text-white-400"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        coverletter_back_end_developer.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-white-400">
-                        4.5mb
-                      </span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      View
-                    </a>
-                  </div>
-                </li>
-              </ul>
-            </dd>
-          </div>
-        </dl>
-      </div>
+                <span className="sr-only">Agree to policies</span>
+                <span
+                  aria-hidden="true"
+                  className="h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out group-data-[checked]:translate-x-3.5"
+                />
+              </Switch>
+            </div>
+            <Label className="text-sm leading-6 text-gray-600">
+              By selecting this, you agree to our{" "}
+              <a href="#" className="font-semibold text-indigo-600">
+                privacy&nbsp;policy
+              </a>
+              .
+            </Label>
+          </Field>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Let's talk
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
